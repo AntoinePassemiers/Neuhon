4ea84eeb72f39d0965ea6e4fca6eaa41a714f8dc
AntoinePassemiers <apassemi@ulb.ac.be>
2016-12-31 09:02:03 +0100

    Krumhansl's tone profiles

---
+++ src/neuhon/profiles.clj
@@ -0,0 +1,3 @@
+(def major-key-profile (to-array (seq [6.4 2.2 3.5 2.3 4.4 4.1 2.5 5.2 2.4 3.7 2.3 2.9])))
+(def minor-key-profile (to-array (seq [6.4 2.8 3.6 5.4 2.7 3.6 2.6 4.8 4.0 2.7 3.3 3.2])))
+
